99e03ec633e48371b0de3e83ae43b1b1c98ae7d3
jdmatvaz <jdmatvaz@gmail.com>
2017-11-22 13:27:26 +0100

    Creada libreria

---
+++ R/andsport-package.r
@@ -0,0 +1,13 @@
+#' andsport.
+#'
+#'Data about towns, sport facilities and sport organizations in Andalusia.
+#'
+#'To use the datasets:
+#'
+#'Andalusian towns -> data(pueblos)
+#'Andalusian sport facilities -> data(instalaciones)
+#'Andalusian sport organizations -> data(entidades)
+#'
+#' @name andsport
+#' @docType package
+NULL
